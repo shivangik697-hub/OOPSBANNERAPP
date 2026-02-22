a9d8fd6aba7317625bc62ce0a4777ae035dd256b
shivangik697-hub <shivangik697@gmail.com>
2026-02-22 14:44:10 +0530

    UC3

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,11 +1,18 @@
+/*
+*OOPSBannerApp UC3
+*
+*@author Dhanush
+*@version 3.0 
+*/
+
 public class OOPSBannerApp{
     public static void main(String[] args) {
-        System.out.println("   *** " + " " + "     *** " + " " + "   ****** " + " " + "       ***** ");
-        System.out.println(" **   ** " + " " + " **   ** " + " " + " **    ** " + " " + "   **     ** ");
-        System.out.println(" **   ** " + " " + " **   ** " + " " + " **    ** " + " " + " ** ");
-        System.out.println(" **   ** " + " " + " **   ** " + " " + " ****** " + " " + "      ***** ");
-        System.out.println(" **   ** " + " " + " **   ** " + " " + " ** " + " " + "               ** ");
-        System.out.println(" **   ** " + " " + " **   ** " + " " + " ** " + " " + "     **      ** ");
-        System.out.println("   *** " + " " + "     *** " + " " + "   ** " + " " + "        ***** ");
+        System.out.println(String.join(" ","   ***   " , " " , "   ***   " , " " , " ******   " , " " , "   *****     "));
+        System.out.println(String.join(" "," **   ** " , " " , " **   ** " , " " , " **    ** " , " " , "  **    **   "));
+        System.out.println(String.join(" "," **   ** " , " " , " **   ** " , " " , " **    ** " , " " , " **          "));
+        System.out.println(String.join(" "," **   ** " , " " , " **   ** " , " " , " ******   " , " " , "   *****     "));
+        System.out.println(String.join(" "," **   ** " , " " , " **   ** " , " " , " **       " , " " , "         **  "));
+        System.out.println(String.join(" "," **   ** " , " " , " **   ** " , " " , " **       " , " " , " **     **   "));
+        System.out.println(String.join(" ","   ***   " , " " , "   ***   " , " " , " **       " , " " , "   *****     "));
     }
 }
